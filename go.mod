569f8ee23805c157d674f2dee7da8328e12aa362
Moritz Poldrack <ich@moritz-poldrack.de>
2020-10-16 15:31:38 +0200

    yay! more styles!

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gitlab.com/poldi1405/go-indicators
 
 go 1.15
+
+require gitlab.com/poldi1405/go-ansi v1.1.0
--- progress/v2/styles.go
+++ progress/v2/styles.go
@@ -8,31 +8,35 @@ import (
 
 // ProgressStyles contains the styles of the progressbars. Custom styles can
 // easily be appended and used.
-var ProgressStyles = map[string]ProgressStyle{
-	"double":        ProgressStyle{Bar: []string{"="}, Empty: []string{" "}, FrameStart: "[", FrameEnd: "]"},
-	"double-":       ProgressStyle{Bar: []string{"-", "="}, Empty: []string{" "}, FrameStart: "[", FrameEnd: "]"},
-	"single":        ProgressStyle{Bar: []string{"-"}, Empty: []string{" "}, FrameStart: "[", FrameEnd: "]"},
-	"parallelogram": ProgressStyle{Bar: []string{"▰"}, Empty: []string{"▱"}},
-	"spaced-blocks": ProgressStyle{Bar: []string{"▮"}, Empty: []string{"▯"}},
-	"block":         ProgressStyle{Bar: []string{"▏", "▎", "▍", "▌", "▋", "▊", "▉", "█"}, Empty: []string{" "}, FrameStart: "▕", FrameEnd: "▏"},
-	"pacman":        ProgressStyle{Bar: []string{"-"}, Empty: []string{" ", "o"}, Head: []string{"c", ansi.Yellow("C")}, FrameStart: "[", FrameEnd: "]", EmptyStyle: func(o string) string { return ansi.Red(o) }, HeadStyle: func(c string) string { return ansi.Yellow(c) }}
-	"":              ProgressStyle{Bar: []string{"="}, Empty: []string{" "}, FrameStart: "[", FrameEnd: "]"},
+var ProgressStyles = map[string]Style{
+	"double":          Style{Bar: []string{"="}, Empty: []string{" "}, FrameStart: "[", FrameEnd: "]"},
+	"double-":         Style{Bar: []string{"-", "="}, Empty: []string{" "}, FrameStart: "[", FrameEnd: "]"},
+	"single":          Style{Bar: []string{"-"}, Empty: []string{" "}, FrameStart: "[", FrameEnd: "]"},
+	"parallelogram":   Style{Bar: []string{"▰"}, Empty: []string{"▱"}},
+	"spaced-blocks":   Style{Bar: []string{"▮"}, Empty: []string{"▯"}},
+	"braille":         Style{Bar: []string{"⠁", "⠃", "⠇", "⠏", "⠟", "⠿"}, Empty: []string{" "}, FrameStart: "(", FrameEnd: ")"},
+	"block":           Style{Bar: []string{"▏", "▎", "▍", "▌", "▋", "▊", "▉", "█"}, Empty: []string{" "}, FrameStart: "▕", FrameEnd: "▏"},
+	"pacman":          Style{Bar: []string{"-"}, Empty: []string{" ", "o"}, Head: []string{"c", ansi.Yellow("C")}, FrameStart: "[", FrameEnd: "]", EmptyStyle: func(o string) string { return ansi.Red(o) }, HeadStyle: func(c string) string { return ansi.Yellow(c) }},
+	"wget":            Style{Bar: []string{"="}, Empty: []string{" "}, Head: []string{">"}, FrameStart: "[", FrameEnd: "]"},
+	"doublesinglebar": Style{Bar: []string{"=", "-"}, AlternatingBar: true, Empty: []string{" "}, Head: []string{">"}, FrameStart: "[", FrameEnd: "]"},
+	"":                Style{Bar: []string{"="}, Empty: []string{" "}, FrameStart: "[", FrameEnd: "]"},
 }
 
-type ProgressStyle struct {
-	Bar            []string
-	AlternatingBar bool
+// Style acts as a container for the progressbar design
+type Style struct {
+	Bar            []string // The characters used to construct the bar
+	AlternatingBar bool     // The bar is not incremental like `block` but like `doublesinglebar`
 
-	Empty []string
+	Empty []string // The characters used to construct the unfilled part of the progressbar
 
-	Head []string
+	Head []string // The character replacing the last one of the bar, if multiple are given they will be alternated
 
-	FrameStart string
-	FrameEnd   string
+	FrameStart string // A character to print before the bar
+	FrameEnd   string // A character to print after the unfilled part
 
-	BarStyle   func(char string) string
-	HeadStyle  func(char string) string
-	EmptyStyle func(char string) string
+	BarStyle   func(char string) string // A function to apply to Bar characters
+	HeadStyle  func(char string) string // A function to apply to Head characters
+	EmptyStyle func(char string) string // A function to apply to Empty-ish(?) characters
 }
 
 // progressStyleMtx locks the map so there are no concurrent map-accesses
@@ -41,9 +45,9 @@ var progressStyleMtx sync.RWMutex
 // DefineStyle allows registering a custom progress-style to use. If the name
 // is already defined it is overwritten. Directly defining it is possible but
 // not recommended.
-func DefineStyle(name string, characters []string) {
+func DefineStyle(name string, pb Style) {
 	progressStyleMtx.Lock()
 	defer progressStyleMtx.Unlock()
 
-	ProgressStyles[name] = characters
+	ProgressStyles[name] = pb
 }
